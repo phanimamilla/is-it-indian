2bac2824e1a1865bcca789facd0f5d825a6061a6
phanimamilla <mamillaphani11@gmail.com>
2020-07-18 13:41:27 +0530

    Updates

---
+++ precache-manifest.4da51120bfa1810a0979d97b96a43158.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9b420bf148673cc23a3828f6e4fc3c",
+    "url": "/is-it-indian/index.html"
+  },
+  {
+    "revision": "560a35d339c443d330d6",
+    "url": "/is-it-indian/static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "6126b3ddf95a90a3d333",
+    "url": "/is-it-indian/static/css/main.44c6d75b.chunk.css"
+  },
+  {
+    "revision": "560a35d339c443d330d6",
+    "url": "/is-it-indian/static/js/2.377562ad.chunk.js"
+  },
+  {
+    "revision": "df95d0187d1f511ca80856f68e07106b",
+    "url": "/is-it-indian/static/js/2.377562ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6126b3ddf95a90a3d333",
+    "url": "/is-it-indian/static/js/main.df0b0ef2.chunk.js"
+  },
+  {
+    "revision": "b2263e7014ec08bc5cf5",
+    "url": "/is-it-indian/static/js/runtime-main.4da4a758.js"
+  },
+  {
+    "revision": "323691618768082ca93957454e43c62f",
+    "url": "/is-it-indian/static/media/logo.32369161.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/is-it-indian/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/is-it-indian/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/is-it-indian/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/is-it-indian/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
